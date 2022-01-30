e8c76ea887f439de2bca33d45caf53731427c11a
Anugrah Tribima <73867671+Nugrh@users.noreply.github.com>
2022-01-31 00:01:43 +0700

    Responsive

--- js/script.js
+++ js/script.js
@@ -30,4 +30,39 @@ $(document).ready(function(){
         $("#cv").hide();
         $("#contact").fadeIn();
     })
+
+    $("#about-btn-res").click(function() {
+        $("#about").fadeIn();
+        $("#project").hide();
+        $("#cv").hide();
+        $("#contact").hide();
+    })
+
+    $("#project-btn-rev").click(function() {
+        $("#about").hide();
+        $("#project").fadeIn();
+        $("#cv").hide();
+        $("#contact").hide();
+
+        $("#content1").animate({bottom: '512'}, "slow");
+        $("#content2").animate({bottom: '512'}, "slow");
+        $("#content3").animate({bottom: '512'}, "slow");
+    })
+
+    $("#cv-btn-rev").click(function() {
+        $("#about").hide();
+        $("#project").hide();
+        $("#cv").fadeIn();
+        $("#contact").hide();
+    })
+
+    $("#contact-btn-rev").click(function() {
+        $("#about").hide();
+        $("#project").hide();
+        $("#cv").hide();
+        $("#contact").fadeIn();
+    })
+
+
+
 })
